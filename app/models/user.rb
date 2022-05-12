53b10ac841797b15f0f4256ce6dfe9ee55cdf048
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-05-12 16:39:49 +0300

    setup cypress

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
     has_secure_password
+    validates_length_of :password, minimum: 4
+    validates_uniqueness_of :email, :case_sensitive => false, on: :create
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+
+end
+def self.authenticate_with_credentials(input_email, input_password)
+
+    @user = User.find_by(email: input_email.strip.downcase)
+
+    if @user && @user.authenticate(input_password)
+      @user
+    else
+      nil
+    end
+
+  end
 
 end
