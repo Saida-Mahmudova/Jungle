4cdcf582888876285a8626fb38fe5f82ebdad73b
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-05-12 19:36:37 +0300

    user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,13 @@
 class User < ApplicationRecord
-    has_secure_password
-    validates_length_of :password, minimum: 4
-    validates_uniqueness_of :email, :case_sensitive => false, on: :create
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :email, presence: true
+  has_secure_password
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, :password_confirmation, length: { minimum: 4 }
 
-end
 def self.authenticate_with_credentials(input_email, input_password)
-
-    @user = User.find_by(email: input_email.strip.downcase)
-
-    if @user && @user.authenticate(input_password)
-      @user
-    else
-      nil
-    end
-
+  user = User.find_by_email(input_email.strip.downcase)
+  return nil if input_email.nil?
+  return user if user && user.authenticate(input_password)
+    nil
   end
-
 end
