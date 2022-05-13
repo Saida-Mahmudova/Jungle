8c62c67f4f9b032ec3c71df90775a91ddbb9eefd
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-05-13 18:14:36 +0300

    Feature Spec: Product Details

--- cypress/integration/home.spec.rb
+++ cypress/integration/home.spec.rb
@@ -1,12 +1,15 @@
 describe('Homepage testing', () => {
-  it("should visit root", () => {
+  beforeEach(() => {
     cy.visit('/')
+  });
+
+  it("should visit root", () => {
   }) 
 
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
-  
+
   it("There is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
   });
---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,11 @@
+describe('Product testing', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it("can click on a product", () => {
+    cy.get(".products article").should("be.visible");
+    cy.get(".products article").first().click();
+    cy.url().should('include', '/products')
+  });
+})
